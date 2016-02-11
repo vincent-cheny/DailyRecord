9dcbfe28bda0ad68c5a2d985068f75a7af3eaeef
ChenYong <chenyong@ChenYongdeMacBook-Pro.local>
2016-02-11 19:25:41 +0800

    增加titlebar

---
+++ DailyRecord/SettingViewController.swift
@@ -0,0 +1,24 @@
+//
+//  SettingViewController.swift
+//  DailyRecord
+//
+//  Created by ChenYong on 16/2/11.
+//  Copyright © 2016年 LazyPanda. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
