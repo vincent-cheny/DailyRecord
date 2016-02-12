36da61e5a5489f53f94c4823e37f801aedea8e3c
ChenYong <chenyong@ChenYongdeMacBook-Pro.local>
2016-02-12 08:59:23 +0800

    更新四个页面

---
+++ DailyRecord/CalendarViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CalendarViewController.swift
+//  DailyRecord
+//
+//  Created by ChenYong on 16/2/12.
+//  Copyright © 2016年 LazyPanda. All rights reserved.
+//
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = false;
+    }
+    
+}
---
+++ DailyRecord/TemplateViewController.swift
@@ -0,0 +1,27 @@
+//
+//  TemplateViewController.swift
+//  DailyRecord
+//
+//  Created by ChenYong on 16/2/12.
+//  Copyright © 2016年 LazyPanda. All rights reserved.
+//
+
+import UIKit
+
+class TemplateViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBarHidden = false;
+    }
+    
+}
