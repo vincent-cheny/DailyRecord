ce7c1090cad44126316020383e3c8f9f395761e1
ChenYong <chenyong@ChenYongdeMacBook-Pro.local>
2016-02-14 07:40:23 +0800

    更新下拉弹窗逻辑

--- DailyRecord/TemplateViewController.swift
+++ DailyRecord/TemplateViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class TemplateViewController: UIViewController {
     
+    @IBOutlet weak var templateFilterBtn: UIBarButtonItem!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -24,4 +26,23 @@ class TemplateViewController: UIViewController {
         self.navigationController?.navigationBarHidden = false;
     }
     
+    @IBAction func templateFilter(sender: AnyObject) {
+        let alert = UIAlertController(title: nil, message: nil, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "全部", style: UIAlertActionStyle.Default, handler: { (UIAlertAction) -> Void in
+            self.templateFilterBtn.title = "全部"
+        }))
+        alert.addAction(UIAlertAction(title: "黑业", style: UIAlertActionStyle.Default, handler: { (UIAlertAction) -> Void in
+            self.templateFilterBtn.title = "黑业"
+        }))
+        alert.addAction(UIAlertAction(title: "黑业对治", style: UIAlertActionStyle.Default, handler: { (UIAlertAction) -> Void in
+            self.templateFilterBtn.title = "黑业对治"
+        }))
+        alert.addAction(UIAlertAction(title: "白业", style: UIAlertActionStyle.Default, handler: { (UIAlertAction) -> Void in
+            self.templateFilterBtn.title = "白业"
+        }))
+        alert.addAction(UIAlertAction(title: "白业对治", style: UIAlertActionStyle.Default, handler: { (UIAlertAction) -> Void in
+            self.templateFilterBtn.title = "白业对治"
+        }))
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
 }
