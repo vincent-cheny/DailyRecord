b61593e1cd1e85914d90e49d5b48ae81f20ba49d
ChenYong <chenyong@ChenYongdeMacBook-Pro.local>
2016-02-13 09:51:47 +0800

    更新about页面

--- DailyRecord/AboutViewController.swift
+++ DailyRecord/AboutViewController.swift
@@ -1,30 +1,23 @@
 //
-//  SettingViewController.swift
+//  AboutViewController.swift
 //  DailyRecord
 //
-//  Created by ChenYong on 16/2/11.
+//  Created by ChenYong on 16/2/13.
 //  Copyright © 2016年 LazyPanda. All rights reserved.
 //
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class AboutViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let backItem = UIBarButtonItem()
-        backItem.title = ""
-        self.navigationItem.backBarButtonItem = backItem
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
- 
-    override func viewWillAppear(animated: Bool) {
-        self.navigationController?.navigationBarHidden = false;
     }
     
 }
