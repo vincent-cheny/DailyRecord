3437be13fe8eb4d0be8386a40aa91757fd8f9b7a
chenyong1 <chenyong1@xiaomi.com>
2016-04-29 23:18:45 +0800

    补齐页面

--- DailyRecord/AddRemindViewController.swift
+++ DailyRecord/AddRemindViewController.swift
@@ -6,4 +6,22 @@
 //  Copyright © 2016年 LazyPanda. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RealmSwift
+
+class AddRemindViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+}
--- DailyRecord/RemindViewController.swift
+++ DailyRecord/RemindViewController.swift
@@ -11,4 +11,20 @@ import RealmSwift
 
 class RemindViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        let backItem = UIBarButtonItem()
+        backItem.title = ""
+        self.navigationItem.backBarButtonItem = backItem
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+    }
 }
