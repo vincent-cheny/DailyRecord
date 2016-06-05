aea1d82441fd00cbecd6cef0b2c41f244a54b287
chenyong1 <chenyong1@xiaomi.com>
2016-06-05 18:30:32 +0800

    增加修改时间描述设置

---
+++ DailyRecord/ModifyTimeDescriptionViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ModifyTimeDescriptionViewController.swift
+//  DailyRecord
+//
+//  Created by ChenYong on 16/5/29.
+//  Copyright © 2016年 LazyPanda. All rights reserved.
+//
+
+import UIKit
+
+class ModifyTimeDescriptionViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
